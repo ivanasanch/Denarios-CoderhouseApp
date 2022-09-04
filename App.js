415ff81156ab1fc96015312fbb4b2602f9f0fe5f
Ivana Sánchez <ivanasanch@gmail.com>
2022-09-04 20:14:01 -0300

    Primer commit

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hola, Coder!</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text:{
+    color : '#fff', 
+    fontWeight:'bold',
+    fontSize: 40
+  }
 });
